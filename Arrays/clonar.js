b804f7737d8999f19fe171cd49d20c2f9871e3b0
Your Name <alessanrospedroso@outlook.com>
2024-06-12 13:33:23 -0300

    Clonando arrays

---
+++ Arrays/clonar.js
@@ -0,0 +1,9 @@
+const notas = [7,7,8,9]
+const novaListaNotas = [...notas,10]
+
+// novaListaNotas.push(10)
+// const nota = 9.75;
+// const novaNota = nota
+
+console.log(notas)
+console.log(novaListaNotas)
